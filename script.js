732e7c706152e718866cf63d567bb33bdf075feb
Auliviet <112747495+auliviet@users.noreply.github.com>
2024-03-12 17:18:38 +1100

    Create function to generate a grid using JS

---
+++ script.js
@@ -0,0 +1,21 @@
+const GRID_SIZE = 16;
+
+function createGrid(gridSize) {
+// Create a grid on the HTML page of X by X cells, X being the gridSize
+let container = document.querySelector(".container");
+
+    for (let i = 0; i < gridSize; i++) {
+        let row = document.createElement("div");
+        row.classList.add("row");
+
+        for (let j = 0; j < gridSize; j++) {
+            let cell = document.createElement("div");
+            cell.classList.add("cell");
+            row.appendChild(cell);
+        }
+
+        container.appendChild(row);
+    }
+}
+
+createGrid(GRID_SIZE);
